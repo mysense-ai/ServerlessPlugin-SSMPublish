ea2a91a8742eb7582eebe63b98fa4b84b8de2e33
Tim Stevens <tim@san1t1.com>
2025-05-21 11:14:05 +0100

    chore: remove code coverage

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   'transform': { '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js' },
   'testMatch': ['**/*.test.ts'],
   'moduleFileExtensions': [ 'ts', 'tsx', 'js' ],
-  'coverageDirectory': '.coverage',
-  "collectCoverageFrom": ["src/**/*.{js,ts,tsx}"],
   'testPathIgnorePatterns': [
     '/node_modules/',
     // '/build/', // github actions places our build in a build folder which results in jest ignoring our tests
